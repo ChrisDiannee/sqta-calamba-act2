d2a8f2514c6b08bf6df51926ec0bbe3609d1b877
ChrisDiannee <cdcalamba02.cdc@gmail.cm>
2017-06-16 12:16:30 +0800

    first commit

---
+++ filter.js
@@ -0,0 +1,14 @@
+function ShortMessages(messages) {
+  var filtered = messages.filter(function(msg) {
+  	if(msg.message && msg.message.length < 50) {
+  		return true;
+  	}
+  });
+
+  
+  return filtered.map(function(msg) {
+  	return msg.message;
+  })
+}
+
+module.exports = ShortMessages
---
+++ helloword1.js
@@ -0,0 +1,8 @@
+
+
+function myFunction(input1) {
+    var res = input1.toUpperCase();
+    return res;
+}
+
+ module.exports = myFunction
